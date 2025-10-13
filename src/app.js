34d77d03bc019324af1b3b503b26981bb8532894
Alan Castellanos <alancastellanos@Mac-mini-de-Alan.local>
2025-10-13 08:04:26 -0600

    Implementación de Express-validator y Morgan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const userRoutes = require("./routes/user.routes");
 const { initDB } = require("./models/index");
 
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use(morgan("dev"));
 
 app.use("/api/users", userRoutes);
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const { body } = require("express-validator");
 const { getUsers, addUser } = require("../controllers/user.controller");
+const { validateFields } = require("../middlewares/validateFields");
 
 const router = express.Router();
 
+
 router.get("/", getUsers);
-router.post("/", addUser);
+router.post(
+    "/",
+    [    body("nombre").notEmpty().withMessage("El nombre es obligatorio"),
+    body("email").isEmail().withMessage("El correo no es válido"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("La contraseña debe tener al menos 6 caracteres"),
+    validateFields,
+    ], 
+    addUser
+);
 
 module.exports = router;
